b6c8786b14811f2f1c6effe9ae0565350b979801
Serendipity <150626035+ruoqing501@users.noreply.github.com>
2026-04-21 00:50:28 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := device/nubia/NX809J
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
+    FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
 # Dynamic partitions
